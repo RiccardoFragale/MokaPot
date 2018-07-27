aab3b3163c4ec0283a0e39865607ede424aac463
Rich <ric.fra@hotmail.it>
2018-07-27 22:40:43 +0100

    Adds unit test for Espresso feature.

---
+++ src/MokaPot.Testing.Behaviours/EspressoBehavioursTests.cs
@@ -0,0 +1,19 @@
+﻿using MokaPot.Backend.Core.Entities;
+using MokaPot.Backend.Core.Features;
+using NUnit.Framework;
+
+namespace MokaPot.Testing.Behaviours
+{
+    [TestFixture]
+    public class EspressoBehavioursTests
+    {
+        [Test]
+        public void EspressoFeatureShouldReturnTrue()
+        {
+            var espressoFeature = new EspressoFeature();
+            var result = espressoFeature.Execute();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
