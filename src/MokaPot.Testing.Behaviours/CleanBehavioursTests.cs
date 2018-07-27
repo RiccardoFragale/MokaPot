6c69f615c5a3a7c6b6187167b49744d3edd8ef39
Rich <ric.fra@hotmail.it>
2018-07-27 19:45:04 +0100

    Adds testing project and adds test for the Cleaning Feature.

---
+++ src/MokaPot.Testing.Behaviours/CleanBehavioursTests.cs
@@ -0,0 +1,18 @@
+﻿using MokaPot.Backend.Core.Features;
+using NUnit.Framework;
+
+namespace MokaPot.Testing.Behaviours
+{
+    [TestFixture]
+    public class CleanBehavioursTests
+    {
+        [Test]
+        public void CleanFeatureShouldReturnTrueWhenValidInput()
+        {
+            var cleaningFeature = new CleaningFeature();
+            var result = cleaningFeature.Execute();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
