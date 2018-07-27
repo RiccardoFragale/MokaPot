1a95b95941cd2fff2c958f32581416bed07f14f8
Rich <ric.fra@hotmail.it>
2018-07-27 23:06:22 +0100

    Adds unit testing for feature's Units.

--- src/MokaPot.Testing.Behaviours/CleanBehavioursTests.cs
+++ src/MokaPot.Testing.Behaviours/CleanBehavioursTests.cs
@@ -1,4 +1,7 @@
-﻿using MokaPot.Backend.Core.Features;
+﻿using MokaPot.Backend.Core.Entities;
+using MokaPot.Backend.Core.Features;
+using MokaPot.Backend.Core.Units;
+using MokaPot.Frontend.Common;
 using NUnit.Framework;
 
 namespace MokaPot.Testing.Behaviours
@@ -13,6 +16,27 @@ namespace MokaPot.Testing.Behaviours
             var result = cleaningFeature.Execute();
 
             Assert.IsTrue(result);
+        }
+
+        [TestCase(1, 50)]
+        [TestCase(9, 50)]
+        public void CleanFeatureShouldReturnTrueWhenValidInput(int blastSeconds, int waterMl)
+        {
+            var cleaningUnit = new SteamBlastUnit(new CoffeeMachineEntity());
+            UnitResult<CoffeeMachineEntity> result = cleaningUnit.Execute(blastSeconds, waterMl);
+
+            Assert.IsTrue(result.IsSuccessful);
+
+        }
+
+        [TestCase(5, 101)]
+        [TestCase(15, 50)]
+        public void CleanFeatureShouldReturnFalseWhenInvalidInput(int seconds, int waterMl)
+        {
+            var cleaningUnit = new SteamBlastUnit(new CoffeeMachineEntity());
+            UnitResult<CoffeeMachineEntity> result = cleaningUnit.Execute(seconds, waterMl);
+
+            Assert.IsFalse(result.IsSuccessful);
         }
     }
 }
--- src/MokaPot.Testing.Behaviours/EspressoBehavioursTests.cs
+++ src/MokaPot.Testing.Behaviours/EspressoBehavioursTests.cs
@@ -1,5 +1,7 @@
 ﻿using MokaPot.Backend.Core.Entities;
 using MokaPot.Backend.Core.Features;
+using MokaPot.Backend.Core.Units;
+using MokaPot.Frontend.Common;
 using NUnit.Framework;
 
 namespace MokaPot.Testing.Behaviours
@@ -14,6 +16,26 @@ namespace MokaPot.Testing.Behaviours
             var result = espressoFeature.Execute();
 
             Assert.IsTrue(result);
+        }
+
+        [TestCase(6, 50)]
+        [TestCase(8, 60)]
+        public void EspressoUnitShouldReturnTrueWhenValidInputAndCleanMachine(int coffeeGrams, int waterMl)
+        {
+            var espressoUnit = new EspressoUnit(new CoffeeMachineEntity());
+            UnitResult<CoffeeMachineEntity> result = espressoUnit.Execute(coffeeGrams, waterMl);
+
+            Assert.IsTrue(result.IsSuccessful);
+        }
+
+        [TestCase(20, 50)]
+        [TestCase(8, 200)]
+        public void EspressoUnitShouldReturnFalseWhenInvalidInput(int coffeeGrams, int waterMl)
+        {
+            var espressoUnit = new EspressoUnit(new CoffeeMachineEntity());
+            var result = espressoUnit.Execute(coffeeGrams, waterMl);
+
+            Assert.IsFalse(result.IsSuccessful);
         }
     }
 }
